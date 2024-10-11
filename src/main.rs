b69ae124a35c787169ae3644460a63d9fd5785db
Matt Tharp <tharpmatt95@gmail.com>
2024-10-11 08:55:43 -0700

    3D movement

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,47 @@
 use glium::{Display, Surface, Program, implement_vertex, uniform};
 use glium::glutin::{event::{Event, WindowEvent, VirtualKeyCode, ElementState}, event_loop::{ControlFlow, EventLoop}, ContextBuilder, GlProfile, GlRequest, window::WindowBuilder};
+use nalgebra::{Matrix4, Perspective3};
 
 fn main() {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().with_title("Mutetra");
+    let window = WindowBuilder::new().with_title("Genero");
     let context = ContextBuilder::new().with_gl(GlRequest::Latest).with_gl_profile(GlProfile::Core);
     let display = Display::new(window, context, &event_loop).unwrap();
 
     #[derive(Copy, Clone)]
     struct Vertex {
-        position: [f32; 2],
+        position: [f32; 3],
     }
 
     implement_vertex!(Vertex, position);
 
-    let vertex1 = Vertex { position: [-0.05, -0.05] };
-    let vertex2 = Vertex { position: [-0.05,  0.05] };
-    let vertex3 = Vertex { position: [ 0.05,  0.05] };
-    let vertex4 = Vertex { position: [ 0.05, -0.05] };
+    let shape = vec![
+        Vertex { position: [-0.5, -0.5, -0.5] },
+        Vertex { position: [ 0.5, -0.5, -0.5] },
+        Vertex { position: [ 0.5,  0.5, -0.5] },
+        Vertex { position: [-0.5,  0.5, -0.5] },
+        // Other cube faces...
+    ];
 
-    let shape = vec![vertex1, vertex2, vertex3, vertex1, vertex3, vertex4];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    let vertex_shader_src = r#"
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleFan);
+    
+    let program = Program::from_source(&display, "
         #version 140
-        in vec2 position;
-        uniform vec2 translation;
+        in vec3 position;
+        uniform mat4 matrix;
         void main() {
-            gl_Position = vec4(position + translation, 0.0, 1.0);
+            gl_Position = matrix * vec4(position, 1.0);
         }
-    "#;
-
-    let fragment_shader_src = r#"
+    ", "
         #version 140
         out vec4 color;
         void main() {
             color = vec4(1.0, 0.0, 0.0, 1.0);
         }
-    "#;
+    ", None).unwrap();
 
-    let program = Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
-    let mut player_x = 0.0;
-    let mut player_y = 0.0;
+    let mut player_position = [0.0, 0.0, -2.0_f32]; // Starting position
+    let perspective = Perspective3::new(4.0 / 3.0, std::f32::consts::FRAC_PI_2, 0.1, 1024.0);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -55,10 +52,10 @@ fn main() {
                     if let Some(key) = input.virtual_keycode {
                         if input.state == ElementState::Pressed {
                             match key {
-                                VirtualKeyCode::W => player_y += 0.1,
-                                VirtualKeyCode::S => player_y -= 0.1,
-                                VirtualKeyCode::A => player_x -= 0.1,
-                                VirtualKeyCode::D => player_x += 0.1,
+                                VirtualKeyCode::W => player_position[2] += 0.1,
+                                VirtualKeyCode::S => player_position[2] -= 0.1,
+                                VirtualKeyCode::A => player_position[0] -= 0.1,
+                                VirtualKeyCode::D => player_position[0] += 0.1,
                                 _ => (),
                             }
                         }
@@ -68,15 +65,21 @@ fn main() {
             },
             _ => (),
         }
-        
+
+        let view = Matrix4::look_at_rh(
+            &nalgebra::Point3::new(player_position[0], player_position[1], player_position[2]),
+            &nalgebra::Point3::new(player_position[0], player_position[1], player_position[2] + 1.0), // Looking forward
+            &nalgebra::Vector3::y_axis(),
+        );
+
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
 
         let uniforms = uniform! {
-            translation: [player_x as f32, player_y as f32], // Ensure type conversion to f32
+            matrix: <[[f32; 4]; 4]>::from(perspective.to_homogeneous() * view),
         };
 
-        target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        target.draw(&glium::VertexBuffer::new(&display, &shape).unwrap(), &indices, &program, &uniforms, &Default::default()).unwrap();
         target.finish().unwrap();
     });
 }
