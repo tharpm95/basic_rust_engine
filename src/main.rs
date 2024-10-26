adf84c4590185f771fc7eebe623e1740cb508a00
Matt Tharp <tharpmatt95@gmail.com>
2024-10-26 07:25:52 -0700

    Fix texture mapping

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,50 @@ struct Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-1.0, -1.0, -1.0], tex_coords: [0.0, 0.0] },
-    Vertex { position: [ 1.0, -1.0, -1.0], tex_coords: [1.0, 0.0] },
-    Vertex { position: [ 1.0,  1.0, -1.0], tex_coords: [1.0, 1.0] },
-    Vertex { position: [-1.0,  1.0, -1.0], tex_coords: [0.0, 1.0] },
+    // Front face
     Vertex { position: [-1.0, -1.0,  1.0], tex_coords: [0.0, 0.0] },
     Vertex { position: [ 1.0, -1.0,  1.0], tex_coords: [1.0, 0.0] },
     Vertex { position: [ 1.0,  1.0,  1.0], tex_coords: [1.0, 1.0] },
     Vertex { position: [-1.0,  1.0,  1.0], tex_coords: [0.0, 1.0] },
+
+    // Back face
+    Vertex { position: [-1.0, -1.0, -1.0], tex_coords: [1.0, 0.0] },
+    Vertex { position: [-1.0,  1.0, -1.0], tex_coords: [1.0, 1.0] },
+    Vertex { position: [ 1.0,  1.0, -1.0], tex_coords: [0.0, 1.0] },
+    Vertex { position: [ 1.0, -1.0, -1.0], tex_coords: [0.0, 0.0] },
+
+    // Top face
+    Vertex { position: [-1.0, 1.0, -1.0], tex_coords: [0.0, 1.0] },
+    Vertex { position: [-1.0, 1.0,  1.0], tex_coords: [0.0, 0.0] },
+    Vertex { position: [ 1.0, 1.0,  1.0], tex_coords: [1.0, 0.0] },
+    Vertex { position: [ 1.0, 1.0, -1.0], tex_coords: [1.0, 1.0] },
+
+    // Bottom face
+    Vertex { position: [-1.0, -1.0, -1.0], tex_coords: [1.0, 1.0] },
+    Vertex { position: [ 1.0, -1.0, -1.0], tex_coords: [0.0, 1.0] },
+    Vertex { position: [ 1.0, -1.0,  1.0], tex_coords: [0.0, 0.0] },
+    Vertex { position: [-1.0, -1.0,  1.0], tex_coords: [1.0, 0.0] },
+
+    // Right face
+    Vertex { position: [ 1.0, -1.0, -1.0], tex_coords: [1.0, 0.0] },
+    Vertex { position: [ 1.0,  1.0, -1.0], tex_coords: [1.0, 1.0] },
+    Vertex { position: [ 1.0,  1.0,  1.0], tex_coords: [0.0, 1.0] },
+    Vertex { position: [ 1.0, -1.0,  1.0], tex_coords: [0.0, 0.0] },
+
+    // Left face
+    Vertex { position: [-1.0, -1.0, -1.0], tex_coords: [0.0, 0.0] },
+    Vertex { position: [-1.0, -1.0,  1.0], tex_coords: [1.0, 0.0] },
+    Vertex { position: [-1.0,  1.0,  1.0], tex_coords: [1.0, 1.0] },
+    Vertex { position: [-1.0,  1.0, -1.0], tex_coords: [0.0, 1.0] },
 ];
 
 const INDICES: &[u16] = &[
-    0, 1, 2, 2, 3, 0,
-    4, 5, 6, 6, 7, 4,
-    0, 1, 5, 5, 4, 0,
-    2, 3, 7, 7, 6, 2,
-    0, 3, 7, 7, 4, 0,
-    1, 2, 6, 6, 5, 1,
+    0, 1, 2, 2, 3, 0, // front
+    4, 5, 6, 6, 7, 4, // back
+    8, 9, 10, 10, 11, 8, // top
+    12, 13, 14, 14, 15, 12, // bottom
+    16, 17, 18, 18, 19, 16, // right
+    20, 21, 22, 22, 23, 20, // left
 ];
 
 #[repr(C)]
