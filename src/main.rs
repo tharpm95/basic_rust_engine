881b6896d6c712710d05d27e036a973dd523d5ce
Matt Tharp <tharpmatt95@gmail.com>
2024-10-26 02:11:22 -0700

    Grab the cursor and fullscreen

--- src/main.rs
+++ src/main.rs
@@ -349,5 +349,12 @@ async fn run(event_loop: EventLoop<()>, window: winit::window::Window) {
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let monitor = window.primary_monitor();
+    window.set_fullscreen(Some(winit::window::Fullscreen::Borderless(monitor)));
+
+    // Attempt to grab the cursor and make it invisible
+    window.set_cursor_grab(true).unwrap(); // use true to grab the cursor
+    window.set_cursor_visible(false);
+
     pollster::block_on(run(event_loop, window));
 }
